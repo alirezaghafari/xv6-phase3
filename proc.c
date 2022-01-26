8f62ee21195ae7759e7321924e108ece5c466a9b
mohamadreza_hakimi <rezah0160@gmail.com>
2022-01-27 01:48:33 +0330

    modify proc.c

--- proc.c
+++ proc.c
@@ -15,6 +15,7 @@ struct {
 static struct proc *initproc;
 
 int nextpid = 1;
+
 extern void forkret(void);
 extern void trapret(void);
 
@@ -102,6 +103,12 @@ found:
   sp -= sizeof *p->tf;
   p->tf = (struct trapframe*)sp;
 
+  // set creation time (Added)
+  p->creationTime = ticks;
+  p->sleepingTime = 0;
+  p->runningTime = 0;
+  p->readyTime = 0;
+
   // Set up new context to start executing at forkret,
   // which returns to trapret.
   sp -= 4;
@@ -141,6 +148,7 @@ userinit(void)
 
   safestrcpy(p->name, "initcode", sizeof(p->name));
   p->cwd = namei("/");
+  p->priority = 3;   // (Added)
 
   // this assignment to p->state lets other cores
   // run this process. the acquire forces the above
@@ -189,6 +197,7 @@ fork(void)
     return -1;
   }
 
+  
   // Copy process state from proc.
   if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
     kfree(np->kstack);
@@ -199,6 +208,7 @@ fork(void)
   np->sz = curproc->sz;
   np->parent = curproc;
   *np->tf = *curproc->tf;
+  np->priority = 3;
 
   // Clear %eax so that fork returns 0 in the child.
   np->tf->eax = 0;
@@ -263,6 +273,7 @@ exit(void)
 
   // Jump into the scheduler, never to return.
   curproc->state = ZOMBIE;
+
   sched();
   panic("zombie exit");
 }
@@ -319,39 +330,124 @@ wait(void)
 //  - swtch to start running that process
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
-void
+void                                          
 scheduler(void)
-{
-  struct proc *p;
+{  
+  struct proc *p;                         
   struct cpu *c = mycpu();
   c->proc = 0;
-  
+
+  extern int curPolicy;                       
+  extern int curLevel;                        
+  int lastPriority = 0;                       
+  int previousProc[6] = {0};                     
+
   for(;;){
     // Enable interrupts on this processor.
     sti();
-
+  
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
+    switch (curPolicy)
+    {
+    case 0:       // Default
+      for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(p->state != RUNNABLE)
+          continue;
+        exec_process(p, c);
+      }
+      break;
+    
+    case 1:       // Priority queues
+      p = checkQueues(previousProc, &lastPriority, 'f', 0);
+      if(p != 0)
+      {
+        exec_process(p, c);
+      }
+      break;
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+    case 2:       // Reverse priority queues
+      p = checkQueues(previousProc, &lastPriority, 'r', 0);
+      if(p != 0)
+      {
+        exec_process(p, c);
+      }
+      break;
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+    case 3:       // Round-Robin
+      for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(p->state == RUNNABLE)
+        {
+          exec_process(p, c);
+        }
+      }
+      break;
 
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
+    case 4:       // Multilevel Queue
+      if(curLevel % 4 == 0)
+      { 
+        for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+        {
+          if(p->state != RUNNABLE || p->queue != 0)
+            continue;
+          exec_process(p, c);
+          break;
+        }
+      }
+
+      curLevel++;  
+
+      if (curLevel % 4 == 1)
+      {   
+          p = checkQueues(previousProc, &lastPriority, 'f', 1);
+          if(p != 0)
+          { 
+            exec_process(p, c);
+          }
+      }
+
+      curLevel++;  
+      lastPriority = 0; 
+      previousProc[0] = 0;
+      previousProc[1] = 0;
+      previousProc[2] = 0;
+      previousProc[3] = 0;
+      previousProc[4] = 0;
+      previousProc[5] = 0;
+
+      if (curLevel % 4 == 2)
+      {   
+          p = checkQueues(previousProc, &lastPriority, 'r', 2);
+          if(p != 0)
+          { 
+            exec_process(p, c);
+          }          
+      }
+
+      curLevel++;  
+      lastPriority = 0; 
+      previousProc[0] = 0;
+      previousProc[1] = 0;
+      previousProc[2] = 0;
+      previousProc[3] = 0;
+      previousProc[4] = 0;
+      previousProc[5] = 0;
+
+      if(curLevel % 4 == 3)
+      { 
+        for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+        { 
+          if(p->state != RUNNABLE)
+            continue;
+          exec_process(p, c);
+          break;
+        }   
+      }
+
+      curLevel++;  
+      break;
     }
     release(&ptable.lock);
-
   }
 }
 
@@ -385,7 +481,7 @@ sched(void)
 void
 yield(void)
 {
-  acquire(&ptable.lock);  //DOC: yieldlock
+  acquire(&ptable.lock);  //DOC: yieldlock  
   myproc()->state = RUNNABLE;
   sched();
   release(&ptable.lock);
@@ -531,4 +627,246 @@ procdump(void)
     }
     cprintf("\n");
   }
+}
+
+
+/////////////////// METHODS ///////////////////
+
+// excute selected process on cpu (Added)
+int
+exec_process(struct proc* p, struct cpu* c){
+  // // Switch to chosen process.  It is the process's job
+  // // to release ptable.lock and then reacquire it
+  // // before jumping back to us.
+
+  c->proc = p;
+  switchuvm(p);
+  p->state = RUNNING;
+
+  swtch(&(c->scheduler), p->context);
+  switchkvm();
+
+  // // Process is done running for now.
+  // // It should have changed its p->state before coming back.
+  c->proc = 0;
+  return 1;
+}
+
+// check every 6 queues and return high priority process (Added)
+struct proc*
+checkQueues(int *previousProc, int *curPriority, char mode, int queue){
+  struct proc* selectedProc;
+  int priority = 0;
+
+
+  for(int j = 0; j < 6; j++){
+    if(mode == 'f')
+      priority = 1+j;
+
+    else if(mode == 'r')
+      priority = 6-j;
+    
+  
+    for (int i = 0; i < 64; i++) {
+      switch(priority) {
+        case 1:
+          selectedProc = &ptable.proc[(previousProc[0] + i) % 64];
+          if (selectedProc->priority == priority && selectedProc->state == RUNNABLE && selectedProc->queue == queue) {
+            previousProc[0] = (previousProc[0] + 1 + i) % NPROC;
+            *curPriority = priority-1;
+            return selectedProc; 
+          }
+        
+        case 2:
+          selectedProc = &ptable.proc[(previousProc[1] + i) % 64];
+          if (selectedProc->priority == priority && selectedProc->state == RUNNABLE && selectedProc->queue == queue) {
+            previousProc[1] = (previousProc[1] + 1 + i) % NPROC;
+            *curPriority = priority-1;
+            return selectedProc;
+          }
+        
+        case 3:
+          selectedProc = &ptable.proc[(previousProc[2] + i) % 64];
+          if (selectedProc->priority == priority && selectedProc->state == RUNNABLE && selectedProc->queue == queue) {
+            previousProc[2] = (previousProc[2] + 1 + i) % NPROC;
+            *curPriority = priority-1;
+            return selectedProc;
+          }
+        
+        case 4:
+          selectedProc = &ptable.proc[(previousProc[3] + i) % 64];
+          if (selectedProc->priority == priority && selectedProc->state == RUNNABLE && selectedProc->queue == queue) {
+            previousProc[3] = (previousProc[3] + 1 + i) % NPROC;
+            *curPriority = priority-1;
+            return selectedProc;
+          }
+        
+        case 5:
+          selectedProc = &ptable.proc[(previousProc[4] + i) % 64];
+          if (selectedProc->priority == priority && selectedProc->state == RUNNABLE && selectedProc->queue == queue) {
+            previousProc[4] = (previousProc[4] + 1 + i) % NPROC;
+            *curPriority = priority-1;
+            return selectedProc;
+          }
+        
+        case 6:
+          selectedProc = &ptable.proc[(previousProc[5] + i) % 64];
+          if (selectedProc->priority == priority && selectedProc->state == RUNNABLE  && selectedProc->queue == queue) {
+            previousProc[5] = (previousProc[5] + 1 + i) % NPROC;
+            *curPriority = priority-1;
+            return selectedProc;
+          }
+      }
+    }
+  }
+
+  return 0;
+}
+
+// increase SLEEPING , RUNNABLE, RUNNING process (Added)
+void
+increase_time(void)
+{
+  struct proc *p;
+  acquire(&ptable.lock);
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+  {
+    if (p->state == UNUSED)
+      continue;
+      
+    if (p->state == RUNNING)
+      p->runningTime++;
+
+    else if (p->state == SLEEPING)
+      p->sleepingTime++;
+
+    else if (p->state == RUNNABLE)
+      p->readyTime++;
+  }
+  release(&ptable.lock);
+}
+
+
+
+// set or modify priority of current process (Added)
+int
+setPriority(int priority)
+{
+  struct proc *curproc = myproc();
+
+  if(priority > 6 || priority < 1)
+  {
+    curproc->priority = 5;
+    return 1;
+  }
+
+  curproc->priority = priority;
+  return 1;
+}
+
+// return priority of current process (Added)
+int
+getPriority(void)
+{
+  struct proc *curproc = myproc();
+  return curproc->priority;
+}
+
+// this syscall writes the related times of specified process into *times (Added)
+int
+wait2(Times *times)
+{
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+
+        // writing the related times (Added)
+        times->sleepingTime = times->sleepingTime;
+        times->creationTime = p->creationTime;
+        times->readyTime = p->readyTime;
+        times->runningTime = p->runningTime;
+        // done writing
+
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+
+// this function fork process to given queue (Added)
+int
+fork2(int queue)
+{
+  int i, pid;
+  struct proc *np;
+  struct proc *curproc = myproc();
+
+  // Allocate process.
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  np->queue = queue;            // (Added)
+  
+  // Copy process state from proc.
+  if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
+    kfree(np->kstack);
+    np->kstack = 0;
+    np->state = UNUSED;
+    return -1;
+  }
+  np->sz = curproc->sz;
+  np->parent = curproc;
+  *np->tf = *curproc->tf;
+  np->priority = 3;
+
+  // Clear %eax so that fork returns 0 in the child.
+  np->tf->eax = 0;
+
+  for(i = 0; i < NOFILE; i++)
+    if(curproc->ofile[i])
+      np->ofile[i] = filedup(curproc->ofile[i]);
+  np->cwd = idup(curproc->cwd);
+
+  safestrcpy(np->name, curproc->name, sizeof(curproc->name));
+
+  pid = np->pid;
+
+  acquire(&ptable.lock);
+
+  np->state = RUNNABLE;
+
+  release(&ptable.lock);
+
+  return pid;
 }
